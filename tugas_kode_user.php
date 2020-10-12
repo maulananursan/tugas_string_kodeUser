661bdfd0c65a6d109573bfd4ce46377f2579f32b
Maulana Nursan <71365575+maulananursan@users.noreply.github.com>
2020-10-12 11:12:46 +0700

    Add files via upload

--- tugas_kode_user.php
+++ tugas_kode_user.php
@@ -1,10 +1,13 @@
 <?php 
 
+//Belajar membuat username dari gabungan nama_depan+random(1-100)+nama_belakang
+
 $nama = "Maulana Nursan Alfarizi";
 $array_nama = explode(" ", $nama);
 $angka_rand = rand(1,100);
 $kode= $array_nama[0].$angka_rand.$array_nama[2];
 
-print_r($kode);
+echo "Nama : ",$nama,"<br>";
+echo "Kode : ", $kode;
 
  ?>
